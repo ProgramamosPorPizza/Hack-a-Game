3244b4e388e80a402d94bb0cde9514bd241d315b
ziclon93 <ziclon93lh@gmail.com>
2018-04-18 16:03:20 +0200

    Main scene

---
+++ Assets/Scripts/SoundToggleScript.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SoundToggleScript : MonoBehaviour {
+    public Toggle soundToggle;
+
+	public void ActiveToggle()
+    {
+        if (soundToggle.isOn){
+            AudioListener.volume = 1F;
+        }
+        else
+        {
+            AudioListener.volume = 0F;
+        }
+    }
+}
